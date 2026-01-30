f8409a7c6a70141a85884493bc3f2639b9f24fb4
NIKI445 <nikitkuzinn@gmail.com>
2026-01-30 23:21:44 +0300

    2 commit

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -5,4 +5,5 @@ const axiosInstance = axios.create({
 		import.meta.env.VITE_REACT_APP_BACKEND_URI || 'http://localhost:3000',
 })
 
+console.log(import.meta.env.VITE_REACT_APP_BACKEND_URI)
 export default axiosInstance
